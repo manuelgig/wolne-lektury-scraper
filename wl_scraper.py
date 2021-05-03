e2b90ba7256cee36d96c9cb9fdf9de35a05b8ab2
Manuel Gigena <78153906+manuelgig@users.noreply.github.com>
2021-05-03 09:01:36 +0200

    Update wl_scraper.py

--- wl_scraper.py
+++ wl_scraper.py
@@ -21,26 +21,33 @@ def getBookList(author_url,author_name,filetypes=['PDF','MP3']):
     soup = BeautifulSoup(source,'lxml') 
     
     # Get the list of books by the given author hoster at wL (including the books' urls, the urls of their PDF's and MP3's)
-    print('Getting list of books by {} @ {} ...'.format(author_name,author_url))
+    print('Getting list of books by {} @ {} ...'.format(author_name,
+                                                        author_url))
     
     book_temp = []
-    for row in soup.find(class_='plain-list').find_all('a',href=True):        
+    for row in soup.find(class_='plain-list').find_all('a', href=True):        
         # Get book name and the url of the book's site
         book_name = row.string
         print(book_name)
-        site_url = '{}{}'.format(base_url,row['href'])
+        site_url = '{}{}'.format(base_url,
+                                 row['href'])
         
         # Use the site_url to make a new soup and get the url's of requested filetypes
         source_book = urllib.request.urlopen(site_url).read()
-        soup_book = BeautifulSoup(source_book,'lxml') 
+        soup_book = BeautifulSoup(source_book,
+                                  'lxml') 
         
-        temp_dict = {'book_name':book_name,'site_url':site_url}
+        temp_dict = {'book_name':book_name,
+                     'site_url':site_url}
         
-        soup_urls = soup_book.find(class_='book-box-formats').find_all('a',href=True,class_=False)
+        soup_urls = soup_book.find(class_='book-box-formats').find_all('a',
+                                                                       href=True,
+                                                                       class_=False)
         for filetype in filetypes:
             for elem in soup_urls:            
                 if elem.string == filetype:
-                    temp_dict['{}_url'.format(filetype)] = '{}{}'.format(base_url,elem['href'])
+                    temp_dict['{}_url'.format(filetype)] = '{}{}'.format(base_url,
+                                                                         elem['href'])
         
         #append the dictionary to book_temp
         book_temp.append(temp_dict)
@@ -49,10 +56,11 @@ def getBookList(author_url,author_name,filetypes=['PDF','MP3']):
     return pd.DataFrame(book_temp)
 
 # getBookPart downloads the files to a dedicated folder inside an <author_name> folder in the wd. WL stores mp3 files in compressed format, unzip=True will unzip them
-def getBookPart(df,author_name,wd,file_type='PDF',unzip=False):    
+def getBookPart(df, author_name, wd, file_type='PDF', unzip=False):    
     
     # Create the <author_name> folder if it does not exist
-    wd_author = '{}{}/'.format(wd,author_name)
+    wd_author = '{}{}/'.format(wd,
+                               author_name)
     
     if len(df['{}_url'.format(file_type)].dropna())>0:
         
@@ -62,15 +70,19 @@ def getBookPart(df,author_name,wd,file_type='PDF',unzip=False):
     
         print('Downloading the {} files...'.format(file_type))
         
-        if not os.path.exists('{}/{}/'.format(wd_author,file_type)):
-            os.makedirs('{}/{}/'.format(wd_author,file_type))
+        if not os.path.exists('{}/{}/'.format(wd_author, file_type)):
+            os.makedirs('{}/{}/'.format(wd_author,
+                                        file_type))
         
         # Check if there is a folder for the file type, and create one otherwise
         for file_url in df['{}_url'.format(file_type)].dropna():
             try:
-                r = requests.get(file_url, allow_redirects=True)
+                r = requests.get(file_url,
+                                 allow_redirects=True)
                 rep_name = file_url[file_url.rfind('/')+1:]
-                open('{}{}/{}'.format(wd_author,file_type,rep_name), 'wb').write(r.content)
+                open('{}{}/{}'.format(wd_author,
+                                      file_type,
+                                      rep_name), 'wb').write(r.content)
                 df.loc[df['{}_url'.format(file_type)]==file_url, '{}_downloaded'.format(file_type)] = 'Yes'
             
                 if (unzip==True) and (rep_name[-3:] == 'zip'):
@@ -90,7 +102,9 @@ def getBookPart(df,author_name,wd,file_type='PDF',unzip=False):
 # scraperWL is the scraper proper, combining getBookList and getBookPart
 # returns a pandas df with book info, urls and download status
 def scraperWL(author_url,author_name,wd,filetypes=['PDF','MP3'],unzip=False):
-    df = getBookList(site_url,author,wd)
+    df = getBookList(site_url,
+                     author,
+                     wd)
     
     print()
     
